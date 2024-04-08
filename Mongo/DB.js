85f46d9e890272fb7dfb0ed5972a988259bec95b
RamadeviRamkumar <ramdevcse25@gmail.com>
2024-04-08 09:48:39 +0530

    update

--- Mongo/DB.js
+++ Mongo/DB.js
@@ -1,3 +1,4 @@
-module.exports = {'url' : 'mongodb://127.0.0.1:/Data'};
-
-PORT : 5000
+module.exports = { 'url' : 'mongodb+srv://ramdevcse25:govind@cluster0.38ydp1x.mongodb.net/data',
+    'url1' : 'mongodb://127.0.0.1:/Data',
+    };
+PORT = 5000
