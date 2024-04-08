6768373769217bcf667fc4c1497206be04495a10
RamadeviRamkumar <ramdevcse25@gmail.com>
2024-04-08 10:25:48 +0530

    new

--- Mongo/DB.js
+++ Mongo/DB.js
@@ -1,4 +1,4 @@
-module.exports = { 'url' : 'mongodb+srv://ramdevcse25:govind@cluster0.38ydp1x.mongodb.net/data',
+module.exports = { 'url' : 'mongodb+srv://Govindaraj:govind@cluster0.38ydp1x.mongodb.net//data',
     'url1' : 'mongodb://127.0.0.1:/Data',
     };
 PORT = 5000
